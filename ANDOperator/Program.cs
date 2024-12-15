0a6b205f39cda515f89ba92aaaa39b139e9a1b36
Kertu Ojamäe <kertu.ojamae@tktk.ee>
2024-12-15 22:49:51 +0200

    add week5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90; Biology >=90; Chemestry >=90;
+
+int math, biology, chemestry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemestry result:");
+chemestry = Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && biology >= 90 && chemestry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your applications cannot be approved.");
+}
---
+++ OROperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90 OR Chemestry >= 90
+//Math && Chemestry || Math && Biology || Chemestry && Biology
+
+int math, biology, chemestry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter yout Biology result;");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemestry result:");
+chemestry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemestry >= 90) || (math >= 90 && biology >= 90 ) || (chemestry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your applications cannot be approved.");
+}
